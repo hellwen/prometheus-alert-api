b2ed9e579108c72308f44be63b5980cd1c64fde4
Dean.wu <hellwen.wu@gmail.com>
2017-03-07 11:54:37 +0800

    ok

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"os"
 	"log"
-	"errors"
 	"strings"
 	"net/http"
 	"io/ioutil"
@@ -20,14 +19,60 @@ func init() {
 	m.Use(martini.Logger())
 	// Setup routes
 	r := martini.NewRouter()
-	r.Post(`/api/alert`, kapacitorAlert)
+	r.Group("/api/alert", func(r martini.Router) {
+		r.Post("/dba", alertDBA)
+		r.Post("/ops", alertOPS)
+	})
+	// r.Post(`/api/alert/ops`, kapacitorAlert)
+	// r.Post(`/api/alert/dba`, kapacitorAlert)
 	// Add the router action
 	m.Action(r.Handle)
 }
 
-func kapacitorAlert(r *http.Request) {
-	log.Printf("alert...")
+func alertDBA(r *http.Request) {
+	log.Printf("alert to dba...")
 
+        message := kapacitorMessage(r)
+
+	log.Printf("message: %s", message)
+
+	msg_url := getMsgUrl()
+	resp, err := httpPost("6", msg_url, message)
+	if err != nil {
+		log.Println(err)
+		log.Println("Message not sended!")
+	} else if resp != nil {
+		log.Printf("respone: %s", string(resp))
+	}
+}
+
+func alertOPS(r *http.Request) {
+        log.Printf("alert to ops...")
+
+        message := kapacitorMessage(r)
+
+        log.Printf("message: %s", message)
+
+        msg_url := getMsgUrl()
+        resp, err := httpPost("5", msg_url, message)
+        if err != nil {
+                log.Println(err)
+                log.Println("Message not sended!")
+        } else if resp != nil {
+                log.Printf("respone: %s", string(resp))
+        }
+}
+
+func getMsgUrl() string {
+        msg_url := os.Getenv("MSG_URL")
+	if msg_url == "" {
+		log.Printf("msg_url is null! Please set the env var MSG_URL")
+	}
+
+        return msg_url
+}
+
+func kapacitorMessage(r *http.Request) string {
 	defer r.Body.Close()
 	jsbody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -43,30 +88,11 @@ func kapacitorAlert(r *http.Request) {
 
 	// log.Printf("body: %s", body)
 
-	message := js.Get("message").MustString()
-	log.Printf("message: %s", message)
-
-	resp, err := httpPost(message)
-	if err != nil {
-		log.Println(err)
-		log.Println("Message not sended!")
-	} else if resp != nil {
-		log.Printf("respone: %s", string(resp))
-	}
+	return js.Get("message").MustString()
 }
 
-func httpPost(content string) ([]byte, error) {
-        msg_tos := os.Getenv("MSG_TOS")
-	if msg_tos == "" {
-		return nil, errors.New("msg_tos is null! Please set the env var MSG_TOS")
-	}
-
-        msg_url := os.Getenv("MSG_URL")
-	if msg_url == "" {
-		return nil, errors.New("msg_url is null! Please set the env var MSG_URL")
-	}
-
-	resp, err := http.Post(msg_url, "application/x-www-form-urlencoded", strings.NewReader("tos=" + msg_tos + "&content=" + content))
+func httpPost(tos string, url string, content string) ([]byte, error) {
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader("tos=" + tos + "&content=" + content))
 
 	if err != nil {
 		return nil, err
